a9dea639a06057cc0b0c0eb422f865b90cf46461
wise-king-sullyman <a.m.sullivan.github@gmail.com>
2020-10-21 20:43:20 -0400

    Added Board class and specs

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Player
+  attr_accessor :name, :symbol
   def initialize(name, symbol)
     @name = name
     @symbol = symbol
@@ -22,5 +23,30 @@ class Player
 
   def input_valid?(input)
     input.to_i.between?(1, 7) ? true : false
+  end
+end
+
+class Board
+  def initialize
+    @grid = create_grid
+  end
+
+  def create_grid
+    blank_grid = []
+    7.times { blank_grid.push([]) }
+    blank_grid.each { |row| 6.times { row.push(0) } }
+  end
+
+  def to_s
+    output_string = ''
+    @grid.transpose.each { |row| output_string += (row.to_s + "\n") }
+    output_string
+  end
+
+  def update(column, symbol)
+    empty_rows = @grid[column].count(0)
+    return puts 'column full' if empty_rows.zero?
+
+    @grid[column][empty_rows - 1] = symbol
   end
 end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -6,7 +6,7 @@ describe Player do
   before do
     allow($stdout).to receive(:write)
   end
-  
+
   subject { Player.new('player 1', '@')}
   describe '#request_input' do
     it 'asks the user for input' do
@@ -43,6 +43,68 @@ describe Player do
       it 'returns column choice' do
         allow(subject).to receive(:gets).and_return('foo', 'bar', 7.to_s)
         expect(subject.choose_column).to eql(6)
+      end
+    end
+  end
+end
+
+describe Board do
+  before do
+    allow($stdout).to receive(:write)
+  end
+  
+  subject { Board.new }
+  
+  describe '#create_grid' do
+    it 'returns a blank grid' do
+      grid_example = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
+      expect(subject.create_grid).to eql(grid_example)
+    end
+  end
+
+  describe '#to_s' do
+    it 'returns grid in a readable pattern' do
+      grid_example = "[0, 0, 0, 0, 0, 0, 0]\n[0, 0, 0, 0, 0, 0, 0]\n[0, 0, 0, 0, 0, 0, 0]\n[0, 0, 0, 0, 0, 0, 0]\n[0, 0, 0, 0, 0, 0, 0]\n[0, 0, 0, 0, 0, 0, 0]\n"
+      expect(subject.to_s).to eql(grid_example)
+    end
+  end
+
+  describe '#update' do
+    let(:player) { Player.new('player 1', '#') }
+    context 'column is empty' do
+      it 'adds symbol to last row of desired column' do
+        grid_example = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, '#'], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
+        subject.update(1, player.symbol)
+        expect(subject.instance_variable_get('@grid')).to eql(grid_example)
+      end
+    end
+
+    context 'column is partially blank' do
+      it 'adds symbol to next available slot in column' do
+        existing_grid = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, '@', '#'], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
+        grid_example = [[0, 0, 0, 0, 0, 0], [0, 0, 0, '#', '@', '#'], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
+        subject.instance_variable_set('@grid', existing_grid)
+        subject.update(1, player.symbol)
+        expect(subject.instance_variable_get('@grid')).to eql(grid_example)
+      end
+    end
+
+    context 'column is full' do
+      it 'does not change the grid' do
+        existing_grid = [[0, 0, 0, 0, 0, 0], ['#', '#', '@', '#', '@', '#'], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
+        grid_example = [[0, 0, 0, 0, 0, 0], ['#', '#', '@', '#', '@', '#'], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
+        subject.instance_variable_set('@grid', existing_grid)
+        subject.update(1, player.symbol)
+        expect(subject.instance_variable_get('@grid')).to eql(grid_example)
+      end
+
+      it 'prints column full' do
+        #allow($stdout).to receive(:write)
+        existing_grid = [[0, 0, 0, 0, 0, 0], ['#', '#', '@', '#', '@', '#'], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
+        subject.instance_variable_set('@grid', existing_grid)
+        expect do
+          subject.update(1, player.symbol)
+        end.to output("column full\n").to_stdout
       end
     end
   end
